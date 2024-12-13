ad97c67aebe45b6ba91c227a4e749fe9f915a342
Dave Jazmin <dgmj2022-2616-63492@bicol-u.edu.ph>
2024-12-13 23:00:21 +0800

    file path fixed

--- views/site/parkingslots.php
+++ views/site/parkingslots.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Parking Slots</title>
-    <link rel="stylesheet" href="../web/css/parkingslots.css">
-    <script src="../web/js/parkingslots.js" defer></script>
+    <link rel="stylesheet" href="../../web/css/parkingslots.css">
+    <script src="../../web/js/parkingslots.js" defer></script>
     
 </head>
 <body>
--- views/site/reservations.php
+++ views/site/reservations.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Parking Dashboard</title>
-    <link rel="stylesheet" href="../web/css/reservations.css">
-    <script src="../web/js/reservations.js" defer></script>
+    <link rel="stylesheet" href="../../web/css/reservations.css">
+    <script src="../../web/js/reservations.js" defer></script>
     
 </head>
 <body>
